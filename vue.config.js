5397307a356f345dfd0d570e9b6c81d436c53281
Pignuuu <Pignuuu@gmail.com>
2022-03-14 22:09:38 +0100

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: './',
+  productionSourceMap: false,
+  outputDir: '../dist',
+  assetsDir: 'static',
+  indexPath: 'templates/index.html',
   transpileDependencies: true
 })
